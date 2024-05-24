8fee3ce4d8923d082a02701e30a8cbe663fdec3c
HobyProjects <hobbyprojects.dev@gmail.com>
2024-05-24 23:27:40 +0530

    model importing coding

--- src/core/model/model.cpp
+++ src/core/model/model.cpp
@@ -1,1 +1,129 @@
 #include "model.hpp"
+
+static void load_mesh(aiMesh *mesh, const aiScene *scene, trimana_core::model *loading_model)
+{
+    static unsigned int mesh_index = 0;
+    trimana_core::mesh *mesh_data = new trimana_core::mesh();
+    mesh_data->mesh_index = mesh_index++;
+
+    // Importing vertices and normals
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+    {
+        glm::vec3 points(mesh->mVertices[i].x, mesh->mVertices[i].y, mesh->mVertices[i].z);
+        mesh_data->vertices[i].points = points;
+
+        glm::vec3 normals(mesh->mNormals[i].x, mesh->mNormals[i].y, mesh->mNormals[i].z);
+        mesh_data->vertices[i].normals = normals;
+    }
+
+    // Color data (for now we go with a default color)
+    for (unsigned int i = 0; i < mesh->mNumVertices; i++)
+    {
+        glm::vec3 color(1.0f, 1.0f, 1.0f);
+        glm::vec4 colorA(1.0f, 1.0f, 1.0f, 1.0f);
+
+        mesh_data->vertices[i].colorRGB = color;
+        mesh_data->vertices[i].colorRGBA = colorA;
+    }
+
+    // Texture coords
+    if (mesh->HasTextureCoords(0))
+    {
+        for (unsigned int i = 0; i < AI_MAX_NUMBER_OF_TEXTURECOORDS; i++)
+        {
+            // We are double checking here becuse we don't want to loop until AI_MAX_NUMBER_OF_TEXTURECOORDS
+            if (mesh->HasTextureCoords(i))
+            {
+                for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+                {
+                    glm::vec2 tex_uv(mesh->mTextureCoords[i][j].x, mesh->mTextureCoords[i][j].y);
+                    mesh_data->vertices[j].uvs = tex_uv;
+                }
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+    else
+    {
+        for (unsigned int j = 0; j < mesh->mNumVertices; j++)
+        {
+            glm::vec2 tex_uv(0.0f, 0.0f);
+            mesh_data->vertices[j].uvs = tex_uv;
+        }
+    }
+
+    // Indices data
+    for(unsigned int i = 0; i < mesh->mNumFaces; i++)
+    {
+        aiFace face = mesh->mFaces[i];
+        for(unsigned int j = 0; j < face.mNumIndices; i++)
+        {
+            mesh_data->indices.emplace_back(face.mIndices[i]);
+        }
+    }
+
+    if(mesh->HasTextureCoords(0))
+    {
+        mesh_data->material_index = mesh->mMaterialIndex;
+        mesh_data->no_texture = false;
+    }
+    else
+    {
+        mesh_data->material_index = GL_ZERO;
+        mesh_data->no_texture = true;
+    }
+
+    loading_model->meshes.emplace_back(mesh_data);
+}
+
+static void load_node(aiNode* node, const aiScene* scene, trimana_core::model* loading_model)
+{
+	for (unsigned int i = 0; i < node->mNumMeshes; i++)
+	{
+		load_mesh(scene->mMeshes[node->mMeshes[i]], scene, loading_model);
+	}
+
+	for (unsigned int i = 0; i < node->mNumChildren; i++)
+	{
+		load_node(node->mChildren[i], scene, loading_model);
+	}
+}
+
+static void QGL_LoadMaterials(const aiScene* scene, trimana_core::model* loading_model)
+{
+	loading_model->textures.resize(scene->mNumMaterials);
+
+	for (unsigned int materialIndex = 0; materialIndex < scene->mNumMaterials; materialIndex++)
+	{
+		aiMaterial* material = scene->mMaterials[materialIndex];
+
+		//TODO: for now we use aiTextureType_DIFFUSE only later we should implement for other texture types
+		if (material->GetTextureCount(aiTextureType_DIFFUSE))
+		{
+			aiString path;
+			if (material->GetTexture(aiTextureType_DIFFUSE, 0, &path) == AI_SUCCESS)
+			{
+				size_t idx = std::string(path.data).rfind("\\");
+				std::string filename = std::string(path.data).substr(idx + 1);
+
+                // [TODO]: enter the correct path
+				std::string texPath = std::string("Resources/Textures/") + filename; 
+				texture_loc texture = trimana_core::load_texture(texPath, trimana_core::COLOR_CHANNELS::RGBA);
+
+				if (!texture)
+				{
+					LOG_ERROR("Unable to load texture : {0} (loading with default textures)", texPath);
+					texture = trimana_core::load_texture("Resources/Textures/plain.png", trimana_core::COLOR_CHANNELS::RGBA);
+					loading_model->textures[materialIndex] = texture;
+					break;
+				}
+
+				LOG_INFO("Texture Loading success : {0}", texPath);
+				loading_model->textures[materialIndex] = texture;
+			}
+		}
+	}
+}
--- src/core/model/model.hpp
+++ src/core/model/model.hpp
@@ -14,16 +14,22 @@ namespace trimana_core
     struct vertexs
     {
         glm::vec3 points;
-        glm::vec3 uvs;
+        glm::vec2 uvs;
         glm::vec3 normals;
-        glm::vec3 color;
+        glm::vec3 colorRGB;
+        glm::vec4 colorRGBA;
     };
 
     struct mesh
     {
+        mesh() = default;
+        ~mesh() = default;
+
         std::vector<vertexs> vertices;
-        vertex_buffers* v_buffers{nullptr};
+        std::vector<unsigned int> indices;
+        
         glm::mat4 mesh_self{1.0f};
+
         unsigned int mesh_index{NULL};
         unsigned int material_index{NULL}; 
         bool no_texture{false};
@@ -33,6 +39,7 @@ namespace trimana_core
     {
         std::vector<mesh*> meshes;
         std::vector<texture_loc> textures;
+        vertex_buffers* buffers{nullptr};
         shader* shader_program{nullptr};
         bool ready_to_draw{false};
     };
